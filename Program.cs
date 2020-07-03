7743145a89c3bc849c426edd8e1866ebb48bb535
unknown <tiagocvalim@gmail.com>
2020-07-03 10:26:04 -0300

    Dotnet New Console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WppConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
